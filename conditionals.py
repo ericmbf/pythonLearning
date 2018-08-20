e69d6580e034aaff4b43090274b7fe8ca39e5bb5
Eric Hall <ericmbf@gmail.com>
2018-08-20 18:13:04 -0300

    Learning about conditionals

--- conditionals.py
+++ conditionals.py
@@ -19,3 +19,23 @@ else:
     fareValue = fareBigCar
 
 print("The fare car is {} to {} car size!".format(fareValue, vehicleSize))
+
+points = 0
+
+points = 200  # use this input to make your submission
+prize = ""
+
+# write your if statement here
+if points <= 50:
+    prize = "wooden rabbit"
+elif points > 50 and points <= 150:
+    result = "Oh dear, no prize this time."
+elif points > 150 and points <= 180:
+    prize = "wafet-thin mint"
+elif points > 180:
+    prize = "penguin"
+    
+if(prize != ""):
+    result = "Congratulations! You won a {}!".format(prize)
+
+print(result)
