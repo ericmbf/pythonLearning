c6a538db7976f600899f48a38bca6b81995090f5
Eric Hall <ericmbf@gmail.com>
2018-09-08 08:13:48 -0300

    Use zip and enumerations

---
+++ zipAndEnumerations.py
@@ -0,0 +1,45 @@
+x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
+y_coord = [677, 233, 405, 433, 905, 376, 432, 445]
+z_coord = [4, 16, -6, -42, 3, -6, 23, -1]
+labels = ["F", "J", "A", "Q", "Y", "B", "W", "X"]
+
+points = []
+# write your for loop here
+for label, x, y, z in zip(labels, x_coord, y_coord, z_coord):
+    points.append("{}: {}, {}, {}".format(label, x, y, z))
+
+
+for point in points:
+    print(point)
+
+cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
+cast_heights = [72, 68, 72, 66, 76]
+
+cast = dict(zip(cast_names, cast_heights))
+print(cast)
+
+cast = (("Barney", 72), ("Robin", 68), ("Ted", 72), ("Lily", 66), ("Marshall", 76))
+
+# define names and heights here
+names, heights = zip(*cast)
+
+print(names)
+print(heights)
+
+data = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11))
+
+data_transpose = zip(*data)
+print(data_transpose)
+
+some_list = [('a', 1), ('b', 2), ('c', 3)]
+letters, nums = zip(*some_list)
+print(letters)
+
+cast = ["Barney Stinson", "Robin Scherbatsky", "Ted Mosby", "Lily Aldrin", "Marshall Eriksen"]
+heights = [72, 68, 72, 66, 76]
+
+# write your for loop here
+for i, name in enumerate(cast):
+    cast[i] = "{} {}".format(cast[i], heights[i])
+
+print(cast)
